4d59038419f6ea85f89f11939e718db382b8bef3
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2018-01-31 01:10:01 -0800

    everything working

--- pset1/q3.py
+++ pset1/q3.py
@@ -42,28 +42,32 @@ def analytical_temp_derivative_planck_function(nu, T):
     return (2 * h**2 * nu**4 / (c**2 * k * T**2)) * factor / (factor - 1)**2
 
 # returns the nu at which the function peaks
-def find_planck_peaks(T, f):
+def find_planck_peaks(T, f, lower = 2):
     # Working in log space
     # Start low, increase until derivative goes negative
     # Backtrack one step to make sure the previous step didn't straddle the peak
     # Halve the step size and keep going until we have constrained it enough
-    lower, step = 1, 0.5
+    step = 0.5
     while True:
-        while f(10**(lower + step), T) > f(10**lower, T):
+        while f(10**(lower + step), T) > f(10**lower, T) or f(10**(lower + step), T) == 0:
             lower = lower + step
         lower -= step
         step /= 2
-        if step < 1e-10:
+        if step < 1e-15:
             return 10**lower
 
 def part_a():
     # assert our numerical integration is good
-    for temp in np.logspace(0, 5):
+    temps = np.power(10, np.arange(5))
+    for temp in temps:
+        res = numerically_integrated_planck_function(temp)
         assert np.isclose(
                 analytically_integrated_planck_function(temp),
-                numerically_integrated_planck_function(temp),
-               rtol=1e-5,
+                res,
+                rtol=1e-5,
         )
+        print("Integral of the Planck function with temperature {}K is {:.6e}".format(
+            temp, res))
 
     # And plot the planck function for fun
     _, ax = plt.subplots()
@@ -75,10 +79,12 @@ def part_a():
     ax.legend()
     plt.show(block=False)
 
+
+
 def part_b():
     # I start running into numerical issues in the brent func at higher temps...
     # My code runs fine, I just have nothing to compare it to.
-    nu_peaks, lambda_peaks, temps = [], [], np.logspace(0, 5)
+    nu_peaks, lambda_peaks, temps = [], [], np.power(10, np.arange(5))
     for temp in temps:
         min_nu, max_nu = nu_limits(temp)
         min_nu = 1 # being reasonable...
@@ -86,47 +92,41 @@ def part_b():
         d_nu_zero = scipy.optimize.brentq(
                 analytical_derivative_planck_function, min_nu, max_nu, args=(temp,),
         )
-        manual_nu_peak = find_planck_peaks(temp, planck_function)
+        manual_nu_peak = find_planck_peaks(temp, planck_function, lower=2)
         assert np.isclose(
                 d_nu_zero,
                 manual_nu_peak,
                rtol=1e-5,
         )
-        nu_peaks.append(planck_function(manual_nu_peak, temp) * (k*temp)/(h*manual_nu_peak))
+        nu_peaks.append(manual_nu_peak)
 
         # Won't do the checking for lambda...
-        manual_lambda_peak = find_planck_peaks(temp, wavelength_planck_function)
-        lambda_peaks.append(wavelength_planck_function(manual_lambda_peak, temp) * (manual_lambda_peak*k*temp) / (h*c))
+        manual_lambda_peak = find_planck_peaks(temp, wavelength_planck_function, lower=-10)
+        lambda_peaks.append(manual_lambda_peak)# * (manual_lambda_peak*k*temp) / (h*c))
 
-    # And plot the derivative for fun
-    _, ax = plt.subplots()
-    for temp in np.power(10, np.arange(5)):
-        nus = np.logspace(4, np.log10(nu_limits(temp)[1]), num=500)
-        ax.plot(nus, analytical_derivative_planck_function(nus, temp), label=temp)
-    ax.set(xscale="log")#, yscale="log")
-    ax.legend()
-    plt.show(block=False)
-
-    # And plot the nu peaks
+    # Plot the nu peaks
     _, ax = plt.subplots()
     ax.plot(temps, nu_peaks)
-    ax.set(xscale="log", yscale="log")
+    ax.set(xscale="log", yscale="log", title="Peak in Frequency vs temp")
     plt.show(block=False)
 
     # And lambda peaks
     _, ax = plt.subplots()
-    ax.plot(temps, nu_peaks)
-    ax.set(xscale="log", yscale="log")
+    ax.plot(temps, lambda_peaks)
+    ax.set(xscale="log", yscale="log", title="Peak in Lambda vs temp")
     plt.show(block=False)
+
+    # And print
+    for i in range(len(nu_peaks)):
+        print("At temp {}K the peak in frequency is at {:.6e}Hz and in wavelength is at {:.6e}cm".format(
+            temps[i], nu_peaks[i], lambda_peaks[i]))
+
 
 def part_c():
-    _, ax = plt.subplots()
     for temp in np.power(10, np.arange(5)):
-        nus = np.logspace(4, np.log10(nu_limits(temp)[1]), num=500)
-        ax.plot(nus, analytical_temp_derivative_planck_function(nus, temp), label=temp)
-    ax.set(xscale="log")#, yscale="log")
-    plt.show(block=False)
-
+        print("Peak for temperature {}K, is at frequency 10^{:.6e}Hz".format(
+            temp, np.log10(find_planck_peaks(temp, analytical_temp_derivative_planck_function)))
+            )
 
 if __name__ == "__main__":
     part_a()
