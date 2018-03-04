df4b64b465f84859242141ad87e220fc20aee80c
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2018-03-03 21:58:21 -0800

    ignore + constants

---
+++ proj/constants.py
@@ -0,0 +1,12 @@
+# Effective recombination coefficient
+# Table 4.11, page 77. cm^3 s^-1
+alpha_eff_2s = 0.838e-13
+
+# Collisional transition rates.
+# Table 4.10, page 76. cm^3 s^-1
+q_p_2s_2p = 1e-4 * (2.51 + 2.23) / 2
+q_e_2s_2p = 1e-4 * (0.22 + 0.35) / 2
+
+# Two photon transition probability
+# Page 73, s^-1
+A_2s_1s = 8.23
